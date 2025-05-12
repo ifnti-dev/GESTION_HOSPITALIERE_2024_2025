bdb611938459ad714def0549b64ac2b67ce29965
Rachad <rachad.djodi@ifnti.com>
2025-05-12 11:25:00 +0000

    Sauvegarde temporaire de mes changements

---
+++ Backend/UeEntreprise(1)/UeEntreprise/src/main/java/com/gestion_hospitaliere/UeEntreprise/UeEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_hospitaliere.UeEntreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeEntrepriseApplication.class, args);
+	}
+
+}
