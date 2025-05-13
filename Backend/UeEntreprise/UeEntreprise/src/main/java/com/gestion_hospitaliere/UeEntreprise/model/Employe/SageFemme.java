fbf33345da641fb719e6924804659abc7972595f
frannoi12 <francoistoyi4@gmail.com>
2025-05-13 08:40:50 +0000

    sages femme

--- Backend/UeEntreprise/UeEntreprise/src/main/java/com/gestion_hospitaliere/UeEntreprise/model/Employe/SageFemme.java
+++ Backend/UeEntreprise/UeEntreprise/src/main/java/com/gestion_hospitaliere/UeEntreprise/model/Employe/SageFemme.java
@@ -5,10 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+
+import java.security.Provider.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.gestion_hospitaliere.UeEntreprise.model.Pregnancy.Accouchement;
 
 @Entity
 public class SageFemme extends Employe {
@@ -23,15 +24,7 @@ public class SageFemme extends Employe {
     @JoinColumn(name = "service_id")
     private Service service;
 
-    // Liste des patientes :
-    // Une relation avec une entité Patiente pour suivre les patientes prises en charge par la sage-femme.
-    // @OneToMany(mappedBy = "sageFemme", cascade = CascadeType.ALL)
-    // private List<Patiente> patientes;
-
-    // Accouchements réalisés :
-    // Une relation avec une entité Accouchement pour suivre les accouchements réalisés par la sage-femme.
-    // @OneToMany(mappedBy = "sageFemme", cascade = CascadeType.ALL)
-    // private List<Accouchement> accouchements;
+    
 
     // Getters et setters
     public String getSpecialite() {
@@ -66,27 +59,11 @@ public class SageFemme extends Employe {
         this.actif = actif;
     }
 
-    // public Service getService() {
-    //     return service;
-    // }
+    public Service getService() {
+        return service;
+    }
 
-    // public void setService(Service service) {
-    //     this.service = service;
-    // }
-
-    // public List<Patiente> getPatientes() {
-    //     return patientes;
-    // }
-
-    // public void setPatientes(List<Patiente> patientes) {
-    //     this.patientes = patientes;
-    // }
-
-    // public List<Accouchement> getAccouchements() {
-    //     return accouchements;
-    // }
-
-    // public void setAccouchements(List<Accouchement> accouchements) {
-    //     this.accouchements = accouchements;
-    // }
+    public void setService(Service service) {
+        this.service = service;
+    }
 }
