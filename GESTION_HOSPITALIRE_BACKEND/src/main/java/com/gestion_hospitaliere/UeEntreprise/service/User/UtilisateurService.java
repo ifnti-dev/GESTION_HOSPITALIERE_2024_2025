358bfa0bb21d94e3090087ffb29a551ebe718c97
frannoi12 <francoistoyi4@gmail.com>
2025-05-16 10:11:28 +0000

    Fin de tout se qui conserne user et employé

---
+++ GESTION_HOSPITALIRE_BACKEND/src/main/java/com/gestion_hospitaliere/UeEntreprise/service/User/UtilisateurService.java
@@ -0,0 +1,59 @@
+package com.gestion_hospitaliere.UeEntreprise.service.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gestion_hospitaliere.UeEntreprise.model.User.Utilisateur;
+import com.gestion_hospitaliere.UeEntreprise.repository.User.UtilisateurRepository;
+
+@Service
+public class UtilisateurService {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    // Ajouter un utilisateur
+    public Utilisateur ajouterUtilisateur(Utilisateur utilisateur) {
+        return utilisateurRepository.save(utilisateur);
+    }
+
+    // Récupérer tous les utilisateurs
+    public List<Utilisateur> obtenirTousLesUtilisateurs() {
+        return utilisateurRepository.findAll();
+    }
+
+    // Récupérer un utilisateur par ID
+    public Optional<Utilisateur> obtenirUtilisateurParId(Long id) {
+        return utilisateurRepository.findById(id);
+    }
+
+    // Récupérer un utilisateur par email
+    public Optional<Utilisateur> obtenirUtilisateurParEmail(String email) {
+        return Optional.ofNullable(utilisateurRepository.findByEmail(email));
+    }
+
+    // Mettre à jour un utilisateur
+    public Utilisateur mettreAJourUtilisateur(Long id, Utilisateur utilisateurDetails) {
+        Optional<Utilisateur> utilisateurOptional = utilisateurRepository.findById(id);
+        if (utilisateurOptional.isPresent()) {
+            Utilisateur utilisateur = utilisateurOptional.get();
+            utilisateur.setNomUtilisateur(utilisateurDetails.getNomUtilisateur());
+            utilisateur.setPassword(utilisateurDetails.getPassword());
+            utilisateur.setNom(utilisateurDetails.getNom());
+            utilisateur.setPrenom(utilisateurDetails.getPrenom());
+            utilisateur.setEmail(utilisateurDetails.getEmail());
+            utilisateur.setRoles(utilisateurDetails.getRoles());
+            return utilisateurRepository.save(utilisateur);
+        } else {
+            throw new RuntimeException("Utilisateur non trouvé avec l'ID : " + id);
+        }
+    }
+
+    // Supprimer un utilisateur
+    public void supprimerUtilisateur(Long id) {
+        utilisateurRepository.deleteById(id);
+    }
+}
